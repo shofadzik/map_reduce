7efcff3d160d38a9ca37113936477080870100ed
Muhammad Shofa Dzikron <107288295+shofadzik@users.noreply.github.com>
2022-09-12 10:42:31 +0700

    Update RatingCounter.py

--- python file/RatingCounter.py
+++ python file/RatingCounter.py
@@ -1,13 +1,11 @@
-#ini contoh untuk mapper reducer (basic)
+from mrjob.job import MRJob 
 
-from mrjob.job import MRJob #import MRjob dari package mrjob.job
-
-class MRRatingCounter (MRJob): #class implemant dari mrjob namanya MRRatingCounter
+class MRRatingCounter (MRJob): 
     def mapper (self, key, line): 
         (userID, movieID, rating, timestamp) = line.split('\t') 
-        yield rating, 1 #yeald ini spt return tapi untuk generator, dan output ini masuk ke input reducer
+        yield rating, 1 
 
-    def reducer(self, rating, occurences): #parameter ini dari mapper diatas
+    def reducer(self, rating, occurences): 
         yield rating, sum(occurences)
 
 if __name__ == '__main__': 
